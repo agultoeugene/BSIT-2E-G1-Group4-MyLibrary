bce01dd7fc480bd2e54295d7da7933916ff15ff0
Eugene Agulto <eugeneagulto5@gmail.com>
2026-03-14 08:10:40 +0800

    adding edit student and make some changes in student inputs

--- pages/student.php
+++ pages/student.php
@@ -64,14 +64,15 @@
                         </div>
 
                         <div class="col-md-6">
-                            <label for="yearGrade" class="form-label">Year / Grade</label>
+                            <label for="yearGrade" class="form-label">Year / Grade (Section)</label>
                             <input type="text" id="yearGrade" class="form-control" />
                             <small class="text-danger" id="errYearGrade"></small>
                         </div>
 
                         <div class="col-md-6">
-                            <label for="course" class="form-label">Course / Strand (Optional)</label>
+                            <label for="course" class="form-label">Course / Strand</label>
                             <input type="text" id="course" class="form-control" />
+                            <small class="text-danger" id="errCourse"></small>
                         </div>
                        <div class="col-md-12">
                         <label for="college" class="form-label">College</label>
@@ -83,30 +84,17 @@
                 </div>
 
                 <div class="modal-footer">
-                    <button class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                    <button type="button" class="btn btn-primary" onclick="addStudent()">
-                            Save Student
-                    </button>
+                        <button class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+
+                        <button class="btn btn-primary" id="addBtn" onclick="addStudent()">
+                            Add Student
+                        </button>
+
+                        <button class="btn btn-success" id="saveBtn" onclick="saveStudent()" style="display:none;">
+                            Save Changes
+                        </button>
                 </div>
 
-            </div>
-        </div>
-    </div>
-
-    <!-- Student Details Modal -->
-    <div class="modal fade" id="studentDetailsModal" tabindex="-1" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header border-0">
-                    <h5 class="modal-title">Student Details</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-                </div>
-                <div class="modal-body text-center">
-                    <h3 id="viewStudentName" class="fw-bold"></h3>
-                    <p class="mb-1"><strong>Student Number:</strong> <span id="viewStudentNumber"></span></p>
-                    <p class="mb-1"><strong>Year / Grade:</strong> <span id="viewYearGrade"></span></p>
-                    <p class="mb-1"><strong>Course / Strand:</strong> <span id="viewCourse"></span></p>
-                </div>
             </div>
         </div>
     </div>
--- script/stud.js
+++ script/stud.js
@@ -1,5 +1,5 @@
 let students = [];
-
+let editRow = null;
 function addStudent() {
     let name = document.getElementById("studentName").value.trim();
     let number = document.getElementById("studentNumber").value.trim();
@@ -10,19 +10,23 @@ function addStudent() {
     let errName = document.getElementById("errStudentName");
     let errNumber = document.getElementById("errStudentNumber");
     let errYear = document.getElementById("errYearGrade");
+    let errCourse = document.getElementById("errCourse");
     let errCollege = document.getElementById("errCollege");
-
+    
     let isValid = true;
 
     errName.innerText = "";
     errNumber.innerText = "";
     errYear.innerText = "";
     errCollege.innerText = "";
+    errCourse.innerText = "";
 
     if (name === "") { errName.innerText = "Student name is required"; isValid = false; }
     if (number === "") { errNumber.innerText = "Student number is required"; isValid = false; }
     if (yearGrade === "") { errYear.innerText = "Year / Grade is required"; isValid = false; }
     if (college === "") { errCollege.innerText = "College is required"; isValid = false; }
+    if (course === "") { errCourse.innerText = "Course/Strand is required"; isValid = false; }
+
 
     if (!isValid) return; 
 
@@ -37,7 +41,10 @@ function addStudent() {
     <td class="text-center">${course}</td>
     <td class="text-center">${college}</td>
     <td class="text-center">
-        <button class="btn btn-danger btn-sm" onclick="deleteStudent(this)">
+         <button class="btn btn-success btn-sm" style="width:70px;" onclick="editStudent(this)">
+             Edit
+        </button>
+        <button class="btn btn-danger btn-sm" style="width:70px;" onclick="deleteStudent(this)">
             Delete
         </button>
     </td>
@@ -51,6 +58,67 @@ function addStudent() {
     const modalInstance = bootstrap.Modal.getInstance(modalEl);
     modalInstance.hide();
 }
+function editStudent(button) {
+
+    editRow = button.closest("tr");
+
+    document.getElementById("studentName").value = editRow.cells[0].innerText;
+    document.getElementById("studentNumber").value = editRow.cells[1].innerText;
+    document.getElementById("yearGrade").value = editRow.cells[2].innerText;
+    document.getElementById("course").value = editRow.cells[3].innerText;
+    document.getElementById("college").value = editRow.cells[4].innerText;
+
+    const modal = new bootstrap.Modal(document.getElementById("addStudentModal"));
+    modal.show();
+
+    document.getElementById("saveBtn").style.display = "block";
+    document.getElementById("addBtn").style.display = "none";
+}
+
+function saveStudent() {
+
+    let name = document.getElementById("studentName").value.trim();
+    let number = document.getElementById("studentNumber").value.trim();
+    let yearGrade = document.getElementById("yearGrade").value.trim();
+    let course = document.getElementById("course").value.trim();
+    let college = document.getElementById("college").value.trim();
+
+    let errName = document.getElementById("errStudentName");
+    let errNumber = document.getElementById("errStudentNumber");
+    let errYear = document.getElementById("errYearGrade");
+    let errCollege = document.getElementById("errCollege");
+    let errCourse = document.getElementById("errCourse");
+
+    let isValid = true;
+
+    errName.innerText = "";
+    errNumber.innerText = "";
+    errYear.innerText = "";
+    errCollege.innerText = "";
+    errCourse.innerText = "";
+
+    if (name === "") { errName.innerText = "Student name is required"; isValid = false; }
+    if (number === "") { errNumber.innerText = "Student number is required"; isValid = false; }
+    if (yearGrade === "") { errYear.innerText = "Year / Grade is required"; isValid = false; }
+    if (college === "") { errCollege.innerText = "College is required"; isValid = false; }
+    if (course === "") { errCourse.innerText = "Course/Strand  is required"; isValid = false; }
+    if (!isValid) return;
+
+    editRow.cells[0].innerText = name;
+    editRow.cells[1].innerText = number;
+    editRow.cells[2].innerText = yearGrade;
+    editRow.cells[3].innerText = course;
+    editRow.cells[4].innerText = college;
+
+    const modalEl = document.getElementById("addStudentModal");
+    const modalInstance = bootstrap.Modal.getInstance(modalEl);
+    modalInstance.hide();
+
+    clearForm();
+
+    document.getElementById("saveBtn").style.display = "none";
+    document.getElementById("addBtn").style.display = "block";
+}
 
 function clearForm(){
     document.getElementById("studentName").value = "";
@@ -58,11 +126,13 @@ function clearForm(){
     document.getElementById("yearGrade").value = "";
     document.getElementById("course").value = "";
     document.getElementById("college").value = "";
+    
 
     document.getElementById("errStudentName").innerText = "";
     document.getElementById("errStudentNumber").innerText = "";
     document.getElementById("errYearGrade").innerText = "";
     document.getElementById("errCollege").innerText = "";
+    document.getElementById("errCourse").innerText = "";
 }
 document.getElementById("studentName").addEventListener("input", () => {
     const err = document.getElementById("errStudentName");
@@ -81,6 +151,10 @@ document.getElementById("yearGrade").addEventListener("input", () => {
 
 document.getElementById("college").addEventListener("input", () => {
     const err = document.getElementById("errCollege");
+    if (err.innerText !== "") err.innerText = "";
+});
+document.getElementById("course").addEventListener("input", () => {
+    const err = document.getElementById("errCourse");
     if (err.innerText !== "") err.innerText = "";
 });
 const addModalEl = document.getElementById("addStudentModal");
