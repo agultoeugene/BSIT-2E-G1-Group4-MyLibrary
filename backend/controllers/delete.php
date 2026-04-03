634d6a28ee8e766ea9819b5768adcbc04d0bd401
Eugene Agulto <eugeneagulto5@gmail.com>
2026-04-03 22:50:42 +0800

    finalizing our system

--- backend/controllers/delete.php
+++ backend/controllers/delete.php
@@ -1,6 +1,8 @@
 <?php
 include("../config/config.php"); 
 
+header('Content-Type: application/json'); // tell browser it's JSON
+
 if(isset($_POST['id'])) {
 
     $id = $_POST['id'];
@@ -9,10 +11,20 @@ if(isset($_POST['id'])) {
     $stmt->bind_param("i", $id);
 
     if($stmt->execute()){
-        echo "success";
+        echo json_encode([
+            'status' => 'success',
+            'message' => 'Account deleted successfully'
+        ]);
     } else {
-        echo "error";
+        echo json_encode([
+            'status' => 'error',
+            'message' => 'Failed to delete account'
+        ]);
     }
 
+} else {
+    echo json_encode([
+        'status' => 'error',
+        'message' => 'No ID provided'
+    ]);
 }
-?>
